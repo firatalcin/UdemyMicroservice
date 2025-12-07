2fbce960dcd98d80fe395ae5ccff5cdc0037b883
Fırat Alçın <firatalcin04@gmail.com>
2025-12-07 16:59:47 +0300

    Course/Category Entity

---
+++ UdemyMicroservice/src/services/catalog/UdemyMicroservice.Catalog.Api/Features/Categories/Category.cs
@@ -0,0 +1,11 @@
+﻿using UdemyMicroservice.Catalog.Api.Features.Courses;
+using UdemyMicroservice.Catalog.Api.Repositories;
+
+namespace UdemyMicroservice.Catalog.Api.Features.Categories
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; } = default!;
+        public List<Course>? Courses { get; set; }
+    }
+}
---
+++ UdemyMicroservice/src/services/catalog/UdemyMicroservice.Catalog.Api/Features/Courses/Course.cs
@@ -0,0 +1,18 @@
+﻿using UdemyMicroservice.Catalog.Api.Features.Categories;
+using UdemyMicroservice.Catalog.Api.Repositories;
+
+namespace UdemyMicroservice.Catalog.Api.Features.Courses
+{
+    public class Course : BaseEntity
+    {
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public decimal Price { get; set; }
+        public Guid UserId { get; set; }
+        public string? Picture { get; set; }
+        public DateTime Created { get; set; }
+
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; } = default!;
+    }
+}
