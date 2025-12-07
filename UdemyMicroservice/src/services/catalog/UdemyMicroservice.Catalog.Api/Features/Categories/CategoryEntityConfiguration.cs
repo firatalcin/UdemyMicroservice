c8804fd69a5df02bd5f72d41b75f40260b8565a0
Fırat Alçın <firatalcin04@gmail.com>
2025-12-07 19:40:16 +0300

    AppDbContext

---
+++ UdemyMicroservice/src/services/catalog/UdemyMicroservice.Catalog.Api/Features/Categories/CategoryEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MongoDB.EntityFrameworkCore.Extensions;
+
+namespace UdemyMicroservice.Catalog.Api.Features.Categories
+{
+    public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToCollection("categories");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedNever();
+            builder.Property(x => x.Name).HasElementName("name").HasMaxLength(100);
+            builder.Ignore(x => x.Courses);
+        }
+    }
+}
---
+++ UdemyMicroservice/src/services/catalog/UdemyMicroservice.Catalog.Api/Repositories/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MongoDB.EntityFrameworkCore.Extensions;
+using System.Reflection;
+using UdemyMicroservice.Catalog.Api.Features.Categories;
+using UdemyMicroservice.Catalog.Api.Features.Courses;
+
+namespace UdemyMicroservice.Catalog.Api.Repositories
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
